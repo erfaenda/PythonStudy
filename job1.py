c66a0c57671ae4dd5b4708aa782ae5bab82ea0bd
erfaenda <regalko7@gmail.com>
2017-08-06 14:28:51 +0500

    лол

---
+++ job1.py
@@ -0,0 +1,22 @@
+twinkeyis = 300
+
+if twinkeyis < 100 or twinkeyis > 500:
+    print('Слишком мало или слишком много')
+else:
+    print('В приделах нормы вкусняшек')
+
+money = 3000
+
+if money <= 100 or money >= 500:
+    print("Не вошел в диапазон 100 или 500")
+else:
+    print('Входит в диапазон')
+
+ninjas = 9
+
+if ninjas >= 50:
+    print("Их слишком много")
+elif ninjas <= 30 and ninjas >= 11 :
+    print('Я вывезу')
+else:
+    print('easy win!')
