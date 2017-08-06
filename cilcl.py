81acaeb8a418447b3e2cc286a351bf08264e5c13
erfaenda <regalko7@gmail.com>
2017-08-06 11:06:44 +0500

    лол

--- cilcl.py
+++ cilcl.py
@@ -1,1 +1,6 @@
-print('ЯСноваПрограмиРую Ура!')
+wizard_list = ['рукоблуд', 'ссанина', 'очко', 'блядун','вагина', 'анус']
+print(wizard_list)
+wizard_list.append('пилотка')
+print(wizard_list)
+wizard_list[5] = 'хуесосина'
+print(wizard_list)
